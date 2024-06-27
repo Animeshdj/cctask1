7016988a8c3251382b7d55bcf8c446fe7ed450e2
Animeshdj <animeshstick.567@gmail.com>
2024-06-27 05:53:55 +0530

    feat: added image download system

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+import { createWriteStream } from "fs";
 import https from "https";
-import http from "http";
 
 function toTitleCase(str) {
   return str.replace(
@@ -8,18 +8,11 @@ function toTitleCase(str) {
   );
 }
 
-let parsedData;
-
-https
-  .get(`https://pokeapi.co/api/v2/pokemon/${process.argv[2]}`, (res) => {
-    const contentType = res.headers["content-type"];
-
-    let error;
-    if (res.statusCode !== 200) {
-      console.log("Pokemon Not Found");
-      return;
-    }
-
+https.get(`https://pokeapi.co/api/v2/pokemon/${process.argv[2]}`, (res) => {
+  if (res.statusCode === 404) {
+    console.log("Pokemon Not Found");
+    return;
+  } else if (res.statusCode === 200) {
     res.setEncoding("utf8");
     let rawData = "";
     res.on("data", (chunk) => {
@@ -27,7 +20,7 @@ https
     });
     res.on("end", () => {
       try {
-        parsedData = JSON.parse(rawData);
+        const parsedData = JSON.parse(rawData);
         console.log("Name:", toTitleCase(parsedData.name));
         console.log("National Number:", parsedData.id);
         console.log(
@@ -40,11 +33,9 @@ https
         );
         console.log(
           "Abilities:",
-          parsedData.abilities
-            .map((ability) => {
-              return toTitleCase(ability.ability.name);
-            })
-            .join(" ")
+          `${parsedData.abilities.map((ability) => {
+            return toTitleCase(ability.ability.name);
+          })}:`
         );
         console.log("Height:", parsedData.height / 10, "m");
         console.log("Weight:", parsedData.weight / 10, "kg", "\n\nBase Stats:");
@@ -53,24 +44,32 @@ https
           console.log(toTitleCase(stat.stat.name).concat(":"), stat.base_stat);
           totalStats += stat.base_stat;
         });
-        console.log("Total:", totalStats);
+        console.log("Total:", totalStats, "\n");
+
+        // console.log(parsedData.sprites.other["official-artwork"].front_default);
+
+        const file = createWriteStream("sprite.png");
+        https.get(
+          parsedData.sprites.other["official-artwork"].front_default,
+          (res) => {
+            res.pipe(file);
+
+            file
+              .on("finish", () => {
+                file.close();
+                console.log("Image Downloaded");
+              })
+              .on("error", (err) => {
+                fs.unlink(imageName);
+                console.error(`Error downloading image: ${err.message}`);
+              });
+          }
+        );
       } catch (e) {
         console.error(e.message);
       }
     });
-  })
-  .on("error", (e) => {
-    console.error(`Got error: ${e.message}`);
-  });
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: parsedData,
-    })
-  );
+  } else {
+    console.log(`Error occurred Status Code: ${res.statusCode}`);
+  }
 });
-// console.log(process.argv[2]);
-
-server.listen(8000);
